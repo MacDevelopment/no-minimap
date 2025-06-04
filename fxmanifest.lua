062fafd2f2d441ec76a15cada749a823b46175e6
Mac <145077500+MacDevelopment@users.noreply.github.com>
2025-06-04 18:32:32 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mac Development'
+description 'Disables the minimap/radar for all players'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
